63556de6ee820ec4f60324b8e4f1a1d4e71915b1
Korawat.Phonyiam <mickky19@gmail.com>
2019-06-05 11:02:27 +0700

    update

--- hellolara/resources/views/korawat.blade.php
+++ hellolara/resources/views/korawat.blade.php
@@ -3,7 +3,7 @@
 
 <div class="title m-b-md">
                     <h3>Korawat</h3>
-                    <img src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.0-9/12705216_1115763045103555_2414811622843992651_n.jpg?_nc_cat=107&_nc_oc=AQl6TGAYM3eoS_ZtebkEMqtEbXTFyQvnxMrVY8tl3w4I8mQC1e2RU1ZWbDsTF_raR-M&_nc_ht=scontent.fbkk10-1.fna&oh=cb8b29d629fdc240d6c4dabfde83b19a&oe=5D928258" alt="myPic" />
+                    <img class="img-circle" src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.0-9/12705216_1115763045103555_2414811622843992651_n.jpg?_nc_cat=107&_nc_oc=AQl6TGAYM3eoS_ZtebkEMqtEbXTFyQvnxMrVY8tl3w4I8mQC1e2RU1ZWbDsTF_raR-M&_nc_ht=scontent.fbkk10-1.fna&oh=cb8b29d629fdc240d6c4dabfde83b19a&oe=5D928258" alt="myPic" />
                 </div>
 
                 <div class="links">
